bb4b441f0cb3c0ba6b43e57a7156291cf9210ac0
Machenike <614368030@qq.com>
2019-02-20 13:04:51 +0800

    1

--- src/main/java/com/jk/SpringbootProviderApplication.java
+++ src/main/java/com/jk/SpringbootProviderApplication.java
@@ -2,6 +2,7 @@ package com.jk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
